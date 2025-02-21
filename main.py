1d32c67f9bfde0270f1dacd0e581cb877cd10cc6
CamiloLopezCO <camilolvh93@gmail.com>
2025-02-21 15:34:16 -0500

    Added funtion to main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def main():
+      
+    # Use the 'with' block to open the file
+    with open("books/frankenstein.txt") as f:
+        # Read the contents of the file
+        file_contents = f.read()
+    
+    # Print the contents to the console
+    print(file_contents)
+
+    main()
