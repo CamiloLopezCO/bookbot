d2b30c14a16db6de69bcab89422cd5df97ea6d54
CamiloLopezCO <camilolvh93@gmail.com>
2025-02-22 17:23:52 -0500

    function get_book_text to main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-def main():
-      
-    # Use the 'with' block to open the file
+def get_book_text():
+    """Reads a file and return its contents as a string."""
+     # Use the 'with' block to open the file
     with open("books/frankenstein.txt") as f:
         # Read the contents of the file
-        file_contents = f.read()
-    
+        return  f.read()
+def main():
+    """Calls get_book_text and prints the book content."""
+    book_text = get_book_text("books/frankenstein.txt")
     # Print the contents to the console
-    print(file_contents)
+    print(book_text)
 
     main()
